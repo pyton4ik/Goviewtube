0b0664190082c73d0025c7a6ccfb141b3f2877b8
Vladimir Sp <trokbrok@yandex.by>
2023-01-27 16:09:45 +0300

    Cut proxy

--- client/composables/proxyUrls.ts
+++ client/composables/proxyUrls.ts
@@ -1,9 +1,9 @@
 export const useProxyUrls = () => {
   const { apiUrl: clientApiUrl } = useApiUrl(true);
   return {
-    textProxy: `${clientApiUrl.value}proxy/text?url=`,
-    imgProxy: `${clientApiUrl.value}proxy/image?url=`,
-    streamProxy: `${clientApiUrl.value}proxy/stream?url=`,
-    videoPlaybackProxy: `${clientApiUrl.value}videoplayback`
+    textProxy: ``,
+    imgProxy: ``,
+    streamProxy: ``,
+    videoPlaybackProxy: ``
   };
 };
